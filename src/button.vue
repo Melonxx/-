f6bca1f8e439ddf468bc5a46f64c815d85378b28
zhangchao <529743595@qq.com>
2018-12-06 16:12:22 +0800

    添加parcel打包工具

---
+++ src/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>按钮</button>
+</template>
+<script>
+  export default {
+    name: 'Button'
+  }
+</script>
+<style lang="scss">
+  button {
+    padding: 10px 20px;
+    font-size: 14px;
+  }
+</style>
